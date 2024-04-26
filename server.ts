dca00f70959dc1ce4cae059df2774f41d94001de
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-04-27 00:00:52 +0530

    added

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log("welcome to ebook api");
+import app from "./backend/app";
+import { PORT } from "./backend/config/config";
+
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log("server listening on port", PORT);
+  });
+};
+
+startServer();
