42ac45e2ff34d492043caeead1b51c94a6442714
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-04-27 10:32:55 +0530

    added user model

--- backend/app.ts
+++ backend/app.ts
@@ -3,6 +3,8 @@ import globalErrorHandler from "./middleware/globalErrorHandler";
 import userRoutes from "./routes/user.routes";
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use("/api/users", userRoutes);
 
--- backend/controllers/user.controllers.ts
+++ backend/controllers/user.controllers.ts
@@ -1,4 +1,24 @@
-import { Request, Response } from "express";
-export const createUserController = async (req: Request, res: Response) => {
+import { Request, Response, NextFunction } from "express";
+import createHttpError from "http-errors";
+import UserModel from "../models/user.models";
+export const createUserController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // validation
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields must be provided");
+    return next(error);
+  }
+
+  // check user already exist in database
+  const user = await UserModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
+    return next(error);
+  }
+
   res.json({ msg: "user created" });
 };
